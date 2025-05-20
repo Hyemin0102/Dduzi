2fd7ea1e453035ee07a53e4c6b7835aa5a62d19e
Hyemin0102 <gkdlt7373@gmail.com>
2025-05-20 18:48:54 +0900

    개인 페이지 컨텐츠 UI 작업

--- src/components/myhome/MyHomeContents.tsx
+++ src/components/myhome/MyHomeContents.tsx
@@ -1,7 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
+import styles from "./MyHomeContents.module.scss";
+import cn from "classnames/bind";
 
+const cx = cn.bind(styles);
 const MyHomeContents = () => {
-  return <div>하단 컨텐츠</div>;
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [kycCertificationInfo, setKycCertificationInfo] = useState<any[]>();
+
+  const temp_post = [
+    {
+      label: "게시물",
+    },
+  ];
+  const temp_bookmark = [
+    {
+      label: "북마크",
+    },
+  ];
+  const temp_favorite = [
+    {
+      label: "즐겨찾기",
+    },
+  ];
+
+  const handleNavigationClick = (index: number) => {
+    setActiveIndex(index);
+    switch (index) {
+      case 0:
+        setKycCertificationInfo(temp_post);
+        break;
+      case 1:
+        setKycCertificationInfo(temp_bookmark);
+        break;
+      case 2:
+        setKycCertificationInfo(temp_favorite);
+        break;
+    }
+  };
+
+  const headerList = ["뜨개일지", "저장일지", "즐겨찾는 뜨친"];
+
+  return (
+    <div className={cx("Wrapper")}>
+      {/** 네비게이션 바 */}
+      <div className={cx("NavigationBar")}>
+        <div className={cx("borderLine")} />
+        {headerList.map((label, index) => (
+          <div>
+            <div
+              key={index}
+              className={cx("NavigationButton", {
+                active: activeIndex === index,
+              })}
+              onClick={() => handleNavigationClick(index)}
+            >
+              {label}
+            </div>
+            {/* <div className={cx("Indicator")} /> */}
+          </div>
+        ))}
+      </div>
+      {kycCertificationInfo &&
+        kycCertificationInfo?.map((item, index) => (
+          <div key={index}>{item.label}</div>
+        ))}
+    </div>
+  );
 };
 
 export default MyHomeContents;
